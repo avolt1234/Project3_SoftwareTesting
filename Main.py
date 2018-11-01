9cebb829f012a92cc4fec6394ac5b18a48f2ad07
Alex Voultos <voultoa@gmail.com>
2018-10-31 21:14:56 -0400

    Alter

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from random import randint
 
-def Main():
+def Main(debug):
 
     scoreCard = {'Aces': 'Empty',
                  'Twos': 'Empty',
@@ -8,8 +8,8 @@ def Main():
                  'Fours': 'Empty',
                  'Fives': 'Empty',
                  'Sixes': 'Empty',
-                 '3oak': 'Empty',
-                 '4oak': 'Empty',
+                 '3-of-a-Kind': 'Empty',
+                 '4-of-a-Kind': 'Empty',
                  'Total Score': 0,
                  'Bonus': 0,
                  'Total': 0,
@@ -23,43 +23,30 @@ def Main():
                  'Total of upper section': 0,
                  'Grand Total': 0
                  }
-
-    while True:
-        rollD = input("\nPress Y to roll dice, press Q to quit: ")
-        if rollD.upper() == 'Y':
-            dice = rollDice()
-
-        breaker = True
-        while breaker:
-            slot = input("Which slot did you want to use? ").title()
-            valid = validator(scoreCard, slot)
-            if valid:
-                breaker = False
-            else:
-                print("The " + slot + " slot is already in use")
-
-        scoreCard = changeScoreCard(scoreCard, slot, dice)
+    if debug.capitalize() == 'T':
+        #TODO create dictionary to parse through
+        scoreCard = changeScoreCard(scoreCard, '3-of-a-Kind', [3, 5, 5, 2, 5])
+        scoreCard = changeScoreCard(scoreCard, '4-of-a-Kind', [3, 5, 5, 5, 5])
         printScorecard(scoreCard)
+    else:
+        while True:
+            rollD = input("\nPress Y to roll dice, press Q to quit: ")
+            if rollD.upper() == 'Y':
+                dice = rollDice()
+
+            breaker = True
+            while breaker:
+                slot = input("Which slot did you want to use? ").title()
+                valid = validator(scoreCard, slot)
+                if valid:
+                    breaker = False
+                else:
+                    print("The " + slot + " slot is already in use")
+
+            scoreCard = changeScoreCard(scoreCard, slot, dice)
+            printScorecard(scoreCard)
 
     return
-
-def MainDebug(scoreCard, diceRolls):
-
-    printScorecard(scoreCard)
-
-    print("\nDice roll is " + str(diceRolls))
-
-    breaker = True
-    while breaker:
-        slot = input("Which slot did you want to use? ").title()
-        valid = validator(scoreCard, slot)
-        if valid:
-            breaker = False
-        else:
-            print("The " + slot + " slot is already in use")
-
-    scoreCard = changeScoreCard(scoreCard, slot, diceRolls)
-    printScorecard(scoreCard)
 
 def rollDice():
 
@@ -82,6 +69,8 @@ def validator(scoreCard, slot):
     try:
         if scoreCard[slot.title()] == 'Empty':
             return True
+        elif slot == 'Yahtzee Bonus' and scoreCard['Yahtzee Bonus'] < 300:
+            return True
         else:
             return False
     except KeyError:
@@ -90,18 +79,23 @@ def validator(scoreCard, slot):
 
 def changeScoreCard(scoreCard, slot, rolls):
 
-    whichOne = {'Aces' : 1, 'Twos' : 2, 'Threes' : 3, 'Fours' : 4, 'Fives' : 5, 'Sixes' : 6}
-
-    mySlot = whichOne[slot]
-
-    holder = 0
-
-    for roll in rolls:
-        if roll == mySlot:
-            holder += roll
-
-    scoreCard[slot] = holder
-
+    if slot == '3-of-a-Kind':
+        newScore = score3OAK(rolls)
+        scoreCard['3-of-a-Kind'] = newScore
+    elif slot == '4-of-a-Kind':
+        newScore = score4OAK(rolls)
+        scoreCard['4-of-a-Kind'] = newScore
+    elif slot == 'Full House':
+        newScore = scoreFullHouse(rolls)
+        scoreCard['Full House'] = newScore
+    else:
+        whichOne = {'Aces' : 1, 'Twos' : 2, 'Threes' : 3, 'Fours' : 4, 'Fives' : 5, 'Sixes' : 6}
+        mySlot = whichOne[slot]
+        holder = 0
+        for roll in rolls:
+            if roll == mySlot:
+                holder += roll
+        scoreCard[slot] = holder
     totalScore = 0
 
     counter = 0
@@ -124,31 +118,44 @@ def printScorecard(scoreCard):
     for key, value in scoreCard.items():
         print(str(key) + '\t' + str(value))
 
-if __name__  == '__main__':
-    debug = input("Enter debug to test program: ")
+def score3OAK(roll):
 
-    if debug.title() == 'Debug':
+    valid = False
 
-        scoreCard = {'Aces': 'Empty', 'Twos': 'Empty', 'Threes': 'Empty', 'Fours': 'Empty', 'Fives': 'Empty',
-                     'Sixes': 'Empty', 'Total Score': 0, 'Bonus': 0, 'Total': 0}
-        while True:
-            whichSlot = input("Enter the slot: ")
-            slotValue = input("Enter the value, leave blank if empty: ")
+    for num in roll:
+        if roll.count(num) == 3:
+            valid = True
 
-            if slotValue:
-                scoreCard[whichSlot] = slotValue
-
-            roller = input("Enter 5 dice rolls, each separated by a comma (1, 2, 3, 4, 5): ")
-
-            diceRoll = [1, 2, 3, 4, 5]
-
-            finalRoll = []
-
-            for char in roller:
-                if char != ',' and char != ' ':
-                    finalRoll.append(int(char))
-            if finalRoll:
-                diceRoll = finalRoll
-            MainDebug(scoreCard, diceRoll)
+    score = 0
+    if valid:
+        for num in roll:
+            score += int(num)
+        return score
     else:
-        Main()
+        return False
+
+def score4OAK(roll):
+    valid = False
+
+    for num in roll:
+        if roll.count(num) == 4:
+            valid = True
+
+    score = 0
+    if valid:
+        for num in roll:
+            score += int(num)
+        return score
+    else:
+        return False
+
+def scoreFullHouse(roll):
+
+    setroll = set(roll)
+
+
+
+
+if __name__  == '__main__':
+    debug = input("Enter T to test program: ")
+    Main(debug)
